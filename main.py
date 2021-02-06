352d8cc71cb108e65cced3b3a1c81278c72cdaeb
Satish Boddu <66247406+satishkumarboddu@users.noreply.github.com>
2021-02-06 21:19:32 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
 import streamlit as st
+from PIL import Image
 
-st.title("Sravi I Love you")
+st.title("Sravi I Love You")
+img = Image.open("img.jpg")
+st.image(img,caption="Sravi", use_column_width=True)
